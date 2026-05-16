21a59592f27ba81c53b9429168b7d51df44cb13e
Arthur Miada <miada.arthur@gmail.com>
2026-05-16 17:11:17 -0300

    codecrafters submit [skip ci]

--- app/builtin.go
+++ app/builtin.go
@@ -14,6 +14,9 @@ type ExitCommand struct{}
 type EchoCommand struct {
 	Args []string
 }
+type TypeCommand struct {
+	Args []string
+}
 
 func (c ExitCommand) Run() {
 	os.Exit(0)
@@ -23,9 +26,19 @@ func (c EchoCommand) Run() {
 	fmt.Println(strings.Join(c.Args[1:], " "))
 }
 
+func (c TypeCommand) Run() {
+	_, ok := ParseBuiltinCommands(c.Args)
+	if ok == nil {
+		fmt.Printf("%s is a shell builtin", c.Args[0])
+	} else {
+		fmt.Printf("%s: not found", c.Args[0])
+	}
+}
+
 const (
 	exit string = "exit"
 	echo string = "echo"
+	type_ string = "type"
 )
 
 func ParseBuiltinCommands(args []string) (bc BuiltinCommands, err error) {
@@ -34,6 +47,8 @@ func ParseBuiltinCommands(args []string) (bc BuiltinCommands, err error) {
 		return ExitCommand{}, nil
 	case "echo":
 		return EchoCommand{args}, nil
+	case "type":
+		return TypeCommand{args}, nil
 	default:
 		return nil, fmt.Errorf("unknown command [%s]", args)
 	}
